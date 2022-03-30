974f060d0c746752f9f4c0475e4d012bba0e17f7
George Dumitru-Stefan <52543506+george4591@users.noreply.github.com>
2022-03-30 10:47:46 +0300

    Update main.c

--- Laboratory/Laboratories/Lab03/main.c
+++ Laboratory/Laboratories/Lab03/main.c
@@ -3,10 +3,30 @@
 #include <memory.h>
 #include <string.h>
 
+typedef struct Student
+{
+	char* name;
+} Student;
 
+typedef struct Node
+{
+	Student* info;
+	Node* pNext;
+} Node;
+
+// function signatures for memory management
+Student* createInfo(char*);
+Node* createNode(Student* info);
+
+// function signatures for list operations
+void insertHead(Node** head, Student* info);
+Node* insertTail(Node* head, Student* info);
+void insertNode(Node* node, Student* info);
+void printList(Node*);
 
 int main()
 {
+	Node* list;
 	FILE* pFile = fopen("Data.txt", "r");
 	char* names[100];
 	memset(names, NULL, 100 * sizeof(char*));
@@ -28,10 +48,94 @@ int main()
 			freq[pos]++;
 			fscanf(pFile, "%s", buffer);
 		}
+
+		char** agenda[26];
+		memset(agenda, NULL, 26 * sizeof(char**));
+
+		short tmp[26];
+		memcpy(tmp, freq, 26 * sizeof(short));
+
+		for (int i = 0; i < index; ++i)
+		{
+			int pos = *names[i] - 'A';
+			if (agenda[pos] == NULL)
+			{
+				agenda[pos] = (char**)malloc(sizeof(char*) * tmp[pos]);
+			}
+
+			agenda[pos][tmp[pos] - 1] = (char*)malloc(sizeof(char) * strlen(names[i]) + 1);
+			strcpy(agenda[pos][tmp[pos] - 1], names[i]);
+			tmp[pos]--;
+		}
+
+		for (int i = 0; i < 26; ++i)
+		{
+			if (agenda[i])
+			{
+				printf("Group %c:\n", i + 'A');
+				for (int j = 0; j < freq[i]; ++j)
+				{
+					printf("\tName: %s\n", agenda[i][j]);
+
+					Student* student = createInfo(agenda[i][j]);
+					insertNode(&list, student);
+					list = insertTail(list, student);
+				}
+			}
+		}
 	}
 
-	char** agenda[26];
-	memset(agenda, NULL, 26 * sizeof(char**));
-
 	return 0;
+}
+
+void insertNode(Node** head, Student* info)
+{
+	// 1,connect node to the structure
+
+	Node* node = createInfo(node);
+	node->pNext = *head;
+
+
+	// 2 connect the structure to the node
+	*head = node;
+}
+
+Node* insertTail(Node* head, Student* info)
+{
+	// 1,connect node to the structure
+
+	Node* node = createInfo(node);
+
+	if (head == NULL)
+	{
+		head = node;
+	}
+	else {
+		// 2 connect the structure to the node
+		Node* tmp = head;
+		while (tmp->pNext)
+		{
+			tmp = tmp->pNext;
+		}
+
+		tmp->pNext = node;
+	}
+
+	return head;
+}
+
+Student* createInfo(char* name)
+{
+	Student* value = (Student*)malloc(sizeof(Student));
+	value->name = (char*)malloc(strlen(name) + 1);
+	strcpy(value->name, name);
+
+	return value;
+}
+
+Node* createNode(Student* info)
+{
+	Node* value = (Node*)malloc(sizeof(Node));
+	value->info = info;
+	value->pNext = NULL;
 }
